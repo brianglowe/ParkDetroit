5454d409f7a58064a020aefc8ccd9b5984b940e8
Ling Toby <asklingtoby@gmail.com>
2015-10-27 14:22:59 -0400

    Pulling in CocoaPod, Adding API

--- ParkDetroit/ParkDetroit/ViewController.swift
+++ ParkDetroit/ParkDetroit/ViewController.swift
@@ -7,12 +7,25 @@
 //
 
 import UIKit
+import GoogleMaps
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        var camera = GMSCameraPosition.cameraWithLatitude(42.3358833,
+            longitude: -83.051934, zoom: 10)
+        var mapView = GMSMapView.mapWithFrame(CGRectZero, camera: camera)
+        mapView.myLocationEnabled = true
+        self.view = mapView
+        
+        var marker = GMSMarker()
+        marker.position = CLLocationCoordinate2DMake(42.3358833,-83.051934)
+        marker.title = "Detroit"
+        marker.snippet = "Michigan"
+        marker.map = mapView
     }
 
     override func didReceiveMemoryWarning() {
---
+++ ParkDetroit/Pods/Target Support Files/Pods-ParkDetroitUITests/Pods-ParkDetroitUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ParkDetroitUITests : NSObject
+@end
+@implementation PodsDummy_Pods_ParkDetroitUITests
+@end
